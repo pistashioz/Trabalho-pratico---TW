d6a5a6f036b195520c6ff736250ed5cb2ffd70ef
Simão Freitas <40220191@esmad.ipp.pt>
2022-12-24 17:41:15 +0000

    Minor changes

--- Contact/contact.js
+++ Contact/contact.js
@@ -1,10 +1,14 @@
+// Dark Theme
 let icon = document.getElementById("icon");
+let logo = document.getElementById("logo");
 icon.onclick = function() {
     document.body.classList.toggle("dark-theme");
     if(document.body.classList.contains("dark-theme")) {
         icon.src = "Imagens/light_mode.png";
+        logo.src = "Imagens/logo-invertido.png";
     } else {
         icon.src = "Imagens/dark_mode.png";
+        logo.src = "Imagens/logo.png";
     }
 
 }
