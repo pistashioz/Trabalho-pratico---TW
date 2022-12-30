7a2e047a9a6b7326f0416b8d374a305141be939f
Victoria Martinez <martinezvictoria781@gmail.com>
2022-12-30 23:07:34 +0000

    i still need to fix the menu

--- siteMulher/main.js
+++ siteMulher/main.js
@@ -1,3 +1,20 @@
+// Dark Theme
+let icon = document.getElementById("icon");
+let logo = document.getElementById("logo");
+icon.onclick = function() {
+    document.body.classList.toggle("dark-theme");
+    if(document.body.classList.contains("dark-theme")) {
+        icon.src = "Images/light_mode.png";
+        logo.src = "Images/logo-invertido.png";
+    } else {
+        icon.src = "Images/dark_mode.png";
+        logo.src = "Images/logo.png";
+    }
+
+}
+
+
+//burguer button
 const menuBtn = document.querySelector('.menu-btn');
 let menuOpen = false;
 menuBtn.addEventListener('click', () =>{
@@ -10,6 +27,7 @@ menuBtn.addEventListener('click', () =>{
     }
 });
 
+//responsive navbar
 
 const navSlide = () => {
     const menuBtn = document.querySelector('.menu-btn');
