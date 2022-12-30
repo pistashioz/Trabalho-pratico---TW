147b1e5c644c8bb77e3488ff85eaf7afc8048596
Victoria Martinez <martinezvictoria781@gmail.com>
2022-12-30 19:04:33 +0000

    burger bar is almost done! need to fix the navbar

--- siteHomem/main.js
+++ siteHomem/main.js
@@ -9,3 +9,27 @@ menuBtn.addEventListener('click', () =>{
         menuOpen = false;
     }
 });
+
+
+const navSlide = () => {
+    const menuBtn = document.querySelector('.menu-btn');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li')
+    //const nav1 = document.querySelector('.logo-center')
+    menuBtn.addEventListener('click', ()=>{
+        //nav1.style.display = "none";
+        //toggle nav
+        nav.classList.toggle('nav-active');
+        //Animate links
+        navLinks.forEach((link, index) =>{
+            if(link.style.animation){
+                link.style.animation = ''
+            }
+            else{
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
+            }
+        });
+    });
+}
+
+navSlide();
