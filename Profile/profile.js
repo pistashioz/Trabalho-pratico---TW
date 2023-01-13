e0fa51ced3a455aa47d60feeb23687350ba392f7
luisaapinho <40220114@esmad.ipp.pt>
2023-01-13 18:00:36 +0000

    menu burguer que nao funciona. no profile.

--- Profile/profile.js
+++ Profile/profile.js
@@ -1,3 +1,6 @@
+//Isso é pra aparecer um de cada vez
+
+
 const tabBtn= document.querySelectorAll(".tab");
 const tab= document.getElementsByClassName("tabShow");
 
@@ -16,3 +19,16 @@ console.log(tab);
         tab[panelIndex].style.display="flex";
 
     }
+
+//Menu burger
+const menuBtn = document.querySelector('.menu-btn');
+let menuOpen = false;
+menuBtn.addEventListener('click', () =>{
+    if (!menuOpen) {
+        menuBtn.classList.add('open');
+        menuOpen = true;
+    } else {
+        menuBtn.classList.remove('open');
+        menuOpen = false;
+    }
+});
