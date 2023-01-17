76f1203c6b3e012d7091825a6fe3c4eb4641ce94
luisaapinho <40220114@esmad.ipp.pt>
2023-01-17 10:56:54 +0000

    match password funcionando o deus grande poderoso

--- cadastro/register.js
+++ cadastro/register.js
@@ -13,45 +13,32 @@ function showPassword() {
   }
 
   //Pra ver se as o confirm senha esta igual
-  /*function validaPassword(){
-    var x= document.getElementsByName("password");
-    var y= document.getElementsByName("password1");
-    console.log("oiii")
-    if(x==y){
-        alert("Account created successfully!");
-        event.preventDefault();
-        window.location.assign("/homepage/main.html");
-        console.log("oi");
-    }
-    else{
-        alert("The passwords do not match :'(")
-    }
-  }(*/
+     
+    let password= document.getElementById("password").value;
+    let confirm= document.getElementById("password1").value;
 
-            // Function to check Whether both passwords
-            // is same or not.
-           /* function checkPassword(form) {
-                password1 = form.password1.value;
-                password2 = form.password2.value;
+    const button=document.getElementById("register");
+    button.addEventListener("click",validaPassword);
+     
+    function validaPassword(){
+    let password= document.getElementById("password").value;
+    let confirm= document.getElementById("password1").value;
+ 
+    console.log("oiii")
+    if(password==""){
+      alert("Field cannot be empty!");
+    }
+    else if(password!=confirm){
+      alert("The passwords do not match :'(")
+      return false
+    }
+    else if(password==confirm){
+      alert("Account created successfully!");
+      window.location.assign("/homepage/main.html");
+      console.log("oi");}
+      return true
+  }
+
   
-                // If password not entered
-                if (password1 == '')
-                    alert ("Please enter Password");
-                      
-                // If confirm password not entered
-                else if (password2 == '')
-                    alert ("Please enter confirm password");
-                      
-                // If Not same return False.    
-                else if (password1 != password2) {
-                    alert ("\nPassword did not match: Please try again...")
-                    return false;
-                }
   
-                // If same return True.
-                else{
-                    alert("Password Match: Welcome to GeeksforGeeks!")
-                    return true;
-                }
-            }*/
 
